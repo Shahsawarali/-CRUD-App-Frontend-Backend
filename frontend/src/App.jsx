8550ad4e5b6ed65fb13fb1de88055cf2a3159d59
IlOvecompUter1$t <shahva2022@gmail.com>
2024-09-28 12:09:53 +0500

    Fetching users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const API_URL = 'http://localhost:3000/users'
+  const API_URL = 'hhttp://localhost:3000/users'
+  const fertchUsers = async () => {
+      const response = await axios.get(API_URL)
+      const content = response.data
+
+  }
 
   return (
     <>
-     
+     <h1>CRUD Application</h1>
     </>
   )
 }
