969180620a77dcac37b125fc170a1d23c1254506
IlOvecompUter1$t <shahva2022@gmail.com>
2024-09-28 12:17:12 +0500

    add users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import axios from 'axios'
 
+const API_URL = 'hhttp://localhost:3000/users'
+
 function App() {
-  const API_URL = 'hhttp://localhost:3000/users'
+  
+  const [users, setusers] = useState([]);
+
   const fertchUsers = async () => {
       const response = await axios.get(API_URL)
       const content = response.data
 
-  }
+      setusers(content.data)
+    }
+
+    useEffect(() => {
+      fertchUsers()
+  }, []);
+
 
   return (
     <>
      <h1>CRUD Application</h1>
+     <input type="text" />
     </>
   )
 }
